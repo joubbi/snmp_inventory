4d7bfb1fba73ff5bc51f5a01111116c5dc3fe116
Farid Joubbi <farid@joubbi.se>
2019-02-18 16:39:43 +0100

    Made if more compatible

--- snmp_inventory.sh
+++ snmp_inventory.sh
@@ -45,32 +45,32 @@ do
 
   # Check if device answers and get general mandatory variables
   $SNMPOPT $LINE .1.3.6.1.2.1.1.5.0 > /dev/null 2>&1
-  if [[ $? == 0 ]]; then
+  if [ $? == 0 ]; then
     hostname=`$SNMPOPT $LINE .1.3.6.1.2.1.1.5.0 | /bin/sed -e 's/\STRING: //g' | /bin/sed -e 's/\"//g' | tr '[<>]' '_'`
     contact=`$SNMPOPT $LINE .1.3.6.1.2.1.1.4.0 | /bin/sed -e 's/\STRING: //g' | /bin/sed -e 's/\"//g' | tr '[<>]' '_'`
     location=`$SNMPOPT $LINE .1.3.6.1.2.1.1.6.0 | /bin/sed -e 's/\STRING: //g' | tr '[<>]' '_'`
 
     #hrDeviceDescr A textual description of this device, including the device's manufacturer and revision, and optionally, its serial number.
     $SNMPOPT $LINE .1.3.6.1.2.1.25.3.2.1.3.1 > /dev/null 2>&1
-    if [[ $? == 0 ]]; then
+    if [ $? == 0 ]; then
       model=`$SNMPOPT $LINE .1.3.6.1.2.1.25.3.2.1.3.1 | /bin/sed -e 's/\STRING: //g' | /bin/sed -e 's/\"//g' | tr '[<>]' '_'`
     else
       $SNMPOPT $LINE .1.3.6.1.2.1.1.2.0 > /dev/null 2>&1
-      if [[ $? == 0 ]]; then
+      if [ $? == 0 ]; then
         sysObjectID=`$SNMPOPT $LINE .1.3.6.1.2.1.1.2.0`
         echo "$sysObjectID" | /bin/grep '.8691.' > /dev/null 
-        if [[ $? == 0 ]]; then
+        if [ $? == 0 ]; then
           model="Moxa"
         else
           $SNMPOPT $LINE .1.3.6.1.2.1.1.1.0 > /dev/null 2>&1
-          if [[ $? == 0 ]]; then
+          if [ $? == 0 ]; then
             sysDescr=`$SNMPOPT $LINE .1.3.6.1.2.1.1.1.0 | /bin/sed -e 's/\STRING: //g' | /bin/sed -e 's/\"//g' | tr '[<>]' '_'`
             echo "$sysDescr" | /bin/grep 'Siemens' > /dev/null
-            if [[ $? == 0 ]]; then
+            if [ $? == 0 ]; then
               model="Siemens"
             fi
             echo "$sysDescr" | /bin/grep 'SCALANCE' > /dev/null
-            if [[ $? == 0 ]]; then
+            if [ $? == 0 ]; then
               model="$sysDescr"
             fi
           fi
@@ -83,15 +83,15 @@ do
     echo "$model" | /bin/grep 'HP' > /dev/null
     if [ $? == 0 ]; then
       $SNMPOPT $LINE .1.3.6.1.2.1.43.5.1.1.17.1 > /dev/null 2>&1
-      if [[ $? == 0 ]]; then
+      if [ $? == 0 ]; then
         serial=`$SNMPOPT $LINE .1.3.6.1.2.1.43.5.1.1.17.1 | /bin/sed -e 's/\STRING: //g' | /bin/sed -e 's/\"//g' | tr '[<>]' '_'`
       fi
       $SNMPOPT $LINE .1.3.6.1.2.1.2.2.1.6.2 > /dev/null 2>&1
-      if [[ $? == 0 ]]; then
+      if [ $? == 0 ]; then
         mac=`$SNMPOPT $LINE .1.3.6.1.2.1.2.2.1.6.2 | /bin/sed -e 's/\STRING: //g' | /bin/sed -e 's/\"//g' | tr '[<>]' '_'`
       else
         $SNMPOPT $LINE .1.3.6.1.2.1.2.2.1.6.3 > /dev/null 2>&1
-        if [[ $? == 0 ]]; then
+        if [ $? == 0 ]; then
           mac=`$SNMPOPT $LINE .1.3.6.1.2.1.2.2.1.6.3 | /bin/sed -e 's/\STRING: //g' | /bin/sed -e 's/\"//g' | tr '[<>]' '_'`
         fi
       fi
@@ -102,7 +102,7 @@ do
     echo "$model" | /bin/grep 'Zebra' > /dev/null
     if [ $? == 0 ]; then
       $SNMPOPT $LINE .1.3.6.1.2.1.2.2.1.6.3 > /dev/null 2>&1
-      if [[ $? == 0 ]]; then
+      if [ $? == 0 ]; then
         mac=`$SNMPOPT $LINE .1.3.6.1.2.1.2.2.1.6.3 | /bin/sed -e 's/\STRING: //g' | /bin/sed -e 's/\"//g' | tr '[<>]' '_'`
       else
         mac=`$SNMPOPT $LINE .1.3.6.1.2.1.2.2.1.6.2 | /bin/sed -e 's/\STRING: //g' | /bin/sed -e 's/\"//g' | tr '[<>]' '_'`
